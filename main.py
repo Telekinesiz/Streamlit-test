6ad35578a127715a7fd0a950a86dac713a03ff26
Telekinesiz <60469985+Telekinesiz@users.noreply.github.com>
2022-04-14 14:33:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ import time
 
 #credentials*********************************************************
 reddit_credentials = praw.Reddit(
-    user_agent=os.getenv('praw_user_agent'),
-    client_id=os.getenv('praw_client_id'),
-    client_secret=os.getenv('praw_client_secret'),
-    username=os.getenv('praw_username'),
+    user_agent=st.secrets('praw_user_agent'),
+    client_id=st.secrets('praw_client_id'),
+    client_secret=st.secrets('praw_client_secret'),
+    username=st.secrets('praw_username'),
     )
 
 headers = {
