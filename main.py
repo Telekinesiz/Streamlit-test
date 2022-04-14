004948b95db200f11489cccd881c7219560bbcc3
Telekinesiz <60469985+Telekinesiz@users.noreply.github.com>
2022-04-14 16:10:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -248,16 +248,16 @@ if __name__ == '__main__':
     panda_clear_data(merged_df, column_for_classification)
     df = panda_clear_data(merged_df, column_for_classification)
     column_with_text = column_for_classification
-    sentiment_classifire(df, column_with_text)
+    #sentiment_classifire(df, column_with_text)
     #topic_categorizer(df, column_with_text, candidate_labels)
 
     dt_object = datetime.fromtimestamp(int(topic_date))
     topic_name_with_url = "["+topic_name+"](https://www.reddit.com/r/place/comments/"+submission_id+"/)"
 
 
-    graf_df_common = df.groupby(['Comment category','Comment label'], sort=False).size().reset_index(name='Count')
-    graf_df_label = df.groupby(['Comment label'], sort=False).size().reset_index(name='Count')
-    graf_df_category = df.groupby(['Comment category'], sort=False).size().reset_index(name='Count')
+    #graf_df_common = df.groupby(['Comment category','Comment label'], sort=False).size().reset_index(name='Count')
+    #graf_df_label = df.groupby(['Comment label'], sort=False).size().reset_index(name='Count')
+    #graf_df_category = df.groupby(['Comment category'], sort=False).size().reset_index(name='Count')
     
     
     
